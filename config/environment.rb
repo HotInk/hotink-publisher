4965bc48baaa6f3593bb6b80f032eab40b159aee
Andrew Louis <andrew@hyfen.net>
2009-03-27 15:53:55 -0400

    Using rdiscount for Markdown output

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,10 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem "rdiscount",
+             :lib    => "rdiscount",
+             :source => "http://gems.github.com"
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
