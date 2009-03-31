b324822e14353b7b8470ebabf3cf77e93224d36f
Andrew Louis <andrew@hyfen.net>
2009-03-31 16:44:51 -0400

    Converted a few more models to ActiveResources.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,8 @@
-class Issue < ActiveRecord::Base
+class Issue < ActiveResource::Base
+
+  # TODO: make this a configuration option
+  # self.site = "http://192.168.1.11:3000"
+  self.site = "http://localhost:3001"
+  self.prefix = "/accounts/:account_id/"
+
 end
