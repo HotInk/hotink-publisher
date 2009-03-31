60e7bc5df49f7fcb524ba2204f25063222380e2a
Andrew Louis <andrew@hyfen.net>
2009-03-31 16:15:23 -0400

    Don't need the wrapper models and migrations for the ActiveRecord versions of accounts/articles

--- app/models/account_resource.rb
+++
@@ -1,8 +0,0 @@
-class AccountResource < ActiveResource::Base
-
-  # TODO: make this a configuration option
-  self.site = "http://192.168.1.11:3000"
-  self.element_name = "account"
-
-  
-end
--- app/models/article_resource.rb
+++
@@ -1,8 +0,0 @@
-class ArticleResource < ActiveResource::Base
-
-  # TODO: make this a configuration option
-  self.site = "http://localhost:3001"
-  self.prefix = "/accounts/:account_id/"
-  self.element_name = "article"
-
-end
