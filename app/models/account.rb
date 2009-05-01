8b86e06a59777d39a613f4c590a06fd2ca2ae61c
Andrew Louis <andrew@hyfen.net>
2009-05-01 15:58:48 -0400

    Added http authentication values for articles/accounts. Still needs a proper configuration system

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,8 @@
 class Account < ActiveResource::Base
 
   # TODO: make this a configuration option
-  self.site = "http://localhost:3001"
+  self.site = "http://192.168.1.1"
+  self.user = "varsitypub"
+  self.password = "blah123"
     
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,10 @@ class Article < ActiveResource::Base
 
   # TODO: make this a configuration option
   # self.site = "http:/session[:user]/192.168.1.11:3000"
-  self.site = "http://localhost:3001"
+  self.site = "http://192.168.1.1"
   self.prefix = "/accounts/:account_id/"
+  self.user = "varsitypub"
+  self.password = "blah123"
 
   def comments
     Comment.find_all_by_content_id(self.id)
