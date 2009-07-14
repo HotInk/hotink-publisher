cb0650d5b67739c3992da88c69703f5ff0629f81
Andrew Louis <andrew@hyfen.net>
2009-07-14 16:20:21 -0400

    mediafile model was in the wrong spot

---
+++ app/models/mediafile.rb
@@ -0,0 +1,8 @@
+class Mediafile < ActiveResource::Base
+
+  self.site = HOTINK_SETTINGS.site
+  self.user = HOTINK_SETTINGS.user
+  self.password = HOTINK_SETTINGS.password
+  self.prefix = "/accounts/:account_id/articles/:article_id/"
+
+end
