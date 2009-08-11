f746b11260cb059ec5808119a35183f3da2a380d
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-11 19:29:36 -0400

    Finally fixed.

--- app/models/template.rb
+++ app/models/template.rb
@@ -3,11 +3,6 @@ class Template < ActiveRecord::Base
   belongs_to :design
   
   # You gotta parse in the controller. Here to save you have to...serialize.
-  def parsed_code=(parsed_template)
-    write_attribute(:parsed_code, Marshal.dump(parsed_template))
-  end
-  def parsed_code
-    Marshal.load(read_attribute(:parsed_code))
-  end
+  serialize :parsed_code, Liquid::Template
   
 end
