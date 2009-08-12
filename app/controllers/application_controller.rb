8801a74921f3bb6df7cc7a3b7e57e6771857842f
Chris Dinn <chris@massaudience.ca>
2009-08-12 09:42:38 -0400

    Added dev credentials.
    Removed backlink from design 'show' page.
    
    Changed array loop in template form to be more explicit and less buggy.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  OAUTH_CREDENTIALS = { :token => "dOZgDp06FMkIjNGQolHYw", :secret => "0jTy6xtVzYL5hKGyYK9ESfw3Ylicygim3WWXj3uaJ3o", :site => HOTINK_SETTINGS.site }
+  #production cred
+  #OAUTH_CREDENTIALS = { :token => "dOZgDp06FMkIjNGQolHYw", :secret => "0jTy6xtVzYL5hKGyYK9ESfw3Ylicygim3WWXj3uaJ3o", :site => HOTINK_SETTINGS.site }
+ 
+  #dev ced
+  OAUTH_CREDENTIALS = { :token => "4iL4tnqIuL8sv2RHQOjQ", :secret => "WSNAza5TIaK7CKggJfSo5cY4MO9xUH7SFItB6gxxE", :site => HOTINK_SETTINGS.site  }
+ 
  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
