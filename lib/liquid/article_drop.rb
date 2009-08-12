cc181f39ddea3daff698be4bf1324afb0eb9e7ff
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-11 22:00:04 -0400

    Small changes.

--- lib/liquid/article_drop.rb
+++ lib/liquid/article_drop.rb
@@ -5,7 +5,7 @@ class Liquid::ArticleDrop < Liquid::BaseDrop
   class_inheritable_reader :liquid_attributes
   write_inheritable_attribute :liquid_attributes, [:id]
 
-  liquid_attributes << :title << :subtitle << :section << :published_at << :id
+  liquid_attributes << :title << :subtitle << :published_at << :id
 
   def initialize(source, options = {})
     super source
@@ -16,6 +16,10 @@ class Liquid::ArticleDrop < Liquid::BaseDrop
   
   def authors_list
     source.authors_list
+  end
+  
+  def section
+    source.section
   end
   
   def id
