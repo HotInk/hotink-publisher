24e440a05423eeba84db773c4c75716d36e509a1
Chris Dinn <chris@massaudience.ca>
2009-08-17 10:19:39 -0400

    Reverted other template files to old paperclip interpolation style (ie. removed ":filename" interpolation)

--- app/models/javascript_file.rb
+++ app/models/javascript_file.rb
@@ -1,4 +1,4 @@
 class JavascriptFile < TemplateFile
   
-  has_attached_file :file, :path => ":rails_root/public/system/:account/:design/javascripts/:version/:filename", :url => "/system/:account/:design/javascripts/:version/:filename", :keep_old_files => :version_condition_met?
+  has_attached_file :file, :path => ":rails_root/public/system/:account/:design/javascripts/:version/:basename.:extension", :url => "/system/:account/:design/javascripts/:version/:basename.:extension", :keep_old_files => :version_condition_met?
 end
--- app/models/stylesheet.rb
+++ app/models/stylesheet.rb
@@ -1,4 +1,4 @@
 class Stylesheet < TemplateFile
-  has_attached_file :file, :path => ":rails_root/public/system/:account/:design/stylesheets/:version/:filename", :url => "/system/:account/:design/stylesheets/:version/:filename", :keep_old_files => :version_condition_met?
+  has_attached_file :file, :path => ":rails_root/public/system/:account/:design/stylesheets/:version/:basename.:extension", :url => "/system/:account/:design/stylesheets/:version/:basename.:extension", :keep_old_files => :version_condition_met?
 
 end
