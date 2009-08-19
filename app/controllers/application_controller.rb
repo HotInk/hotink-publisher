5817be8907199c475eeaec242882504158e396ed
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-19 07:22:21 -0400

    Fixed logic error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -91,7 +91,7 @@ class ApplicationController < ActionController::Base
     def require_user
       logger.info "Current user: " + current_user.class.name
       #First, check to see if user is already logged in
-      unless (current_user && current_user.account==@account) || ( current_user.account.id==1 )
+      unless (current_user && current_user.account==@account) || ( current_user && current_user.account.id==1 )
 
         # Second, check to see if session negotiation is ongoing  
         if params[:session_action]
