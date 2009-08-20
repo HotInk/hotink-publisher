41653228d1355ddef8ecbb7ae00b95e7d0d61320
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-19 20:54:01 -0400

    Made issue articles lazy-loading

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -6,24 +6,8 @@ class Issue < HyperactiveResource
   self.site = HOTINK_SETTINGS.site
   self.prefix = "/accounts/:account_id/"
 
-  def to_liquid
-    {   'date' => self.date,
-        'description' => self.description,
-        'name' => self.name, 
-        'number' => self.number, 
-        'volume' => self.volume, 
-        'articles' => articles,
-        'press_pdf_url' => self.press_pdf_file, 
-        'screen_pdf_url' => self.screen_pdf_file, 
-        'large_cover_image' => self.large_cover_image,
-        'small_cover_image' => self.small_cover_image,
-        'id' => self.id,
-        'account_id' => self.account_id
-    }
-  end
-  
-  def articles
-    articles = Article.find(:all, :from => "/accounts/#{self.account.account_resource_id.to_s}/issues/#{self.id.to_s}/articles.xml", :as => self.account.access_token)
+  def to_liquid(options = {})
+    Liquid::ArticleDrop.new self, options
   end
 
 end
