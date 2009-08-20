dc2dfab9206c6e32bf5f4d3d4ae7525e95720a29
Chris Dinn <chris@massaudience.ca>
2009-08-20 11:33:17 -0400

    Cleaned old active resource info from new OAuth-enabled HyperactiveResources

--- app/models/mediafile.rb
+++ app/models/mediafile.rb
@@ -1,8 +1,6 @@
 class Mediafile < HyperactiveResource
 
   self.site = HOTINK_SETTINGS.site
-  self.user = HOTINK_SETTINGS.user
-  self.password = HOTINK_SETTINGS.password
   # self.prefix = "/accounts/:account_id/articles/:article_id/"
 
   belongs_to :article, :nested => true
