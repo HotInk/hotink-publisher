2c94b207d4174e523a50947d2523351ac443f5c3
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-19 21:05:29 -0400

    Let's add the drop.

---
+++ lib/liquid/issue_drop.rb
@@ -0,0 +1,25 @@
+class Liquid::IssueDrop < Liquid::BaseDrop
+    
+  class_inheritable_reader :liquid_attributes
+  write_inheritable_attribute :liquid_attributes, [:id]
+
+  liquid_attributes << :name << :description  << :number << :volume << :date << :account_id << :id << :large_cover_image << :small_cover_image
+
+  def initialize(source, options = {})
+    super source
+    @options  = options
+  end
+
+  def articles
+      @articles ||= Article.find(:all, :from => "/accounts/#{self.account.account_resource_id.to_s}/issues/#{self.id.to_s}/articles.xml", :as => self.account.access_token)
+  end
+  
+  def press_pdf_url
+    source.press_pdf_file
+  end
+  
+  def screen_pdf_url
+    source.screen_pdf_file
+  end
+
+end
