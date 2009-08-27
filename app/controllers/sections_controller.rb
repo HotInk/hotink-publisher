8c3c14c7c261f2dfec4f70d9e5036750dcc1a5f4
Chris Dinn <chris@massaudience.ca>
2009-08-27 11:09:02 -0400

    Pluralization. It's easy to miss.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -14,7 +14,7 @@ class SectionsController < ApplicationController
     
     @articles = Article.paginate(:all, :page => (params[:page] || 1), :per_page => ( params[:per_page] || 15), :account_id => @account.account_resource_id, :section_id => @section.id, :as => @account.access_token)
     @article_pagination = { :current_page => @articles.first.current_page, :per_page => @articles.first.per_page, :total_entries => @articles.first.total_entries }
-    @article = @articles.first.article
+    @articles = @articles.first.article
     
       # Widget data processing -- start  
       # Build query of only the necessary ids, from the widgets
