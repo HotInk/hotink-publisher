2679bd990b9b69cc6ea9b64a9ff93ea45d4dbda4
Chris Dinn <chris@massaudience.ca>
2009-08-28 10:29:24 -0400

    Added widgets to layout on Section/show.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -21,9 +21,11 @@ class SectionsController < ApplicationController
     @articles = @articles.first.article
     
       # Widget data processing -- start  
-      # Build query of only the necessary ids, from the widgets
+      # Build query of only the necessary ids, from the widgets      
       schema_ids = Array.new
-      @current_template.widgets.each do |widget|
+      found_widgets = @current_template.widgets
+      found_widgets += @current_template.current_layout.widgets if @current_template.current_layout
+      found_widgets.each do |widget|
         widget.schema.each_key do |item|
           schema_ids += widget.schema[item]['ids']
         end
@@ -39,7 +41,7 @@ class SectionsController < ApplicationController
            schema_articles.merge!(article.id.to_s => article)
         end
 
-        @current_template.widgets.each do |widget|
+        found_widgets.each do |widget|
           widget.schema.each_key do |item|
             item_array = widget.schema[item]['ids'].collect{ |i| schema_articles[i] }
             widget_data.merge!( "#{item}_#{widget.name}" => item_array )
