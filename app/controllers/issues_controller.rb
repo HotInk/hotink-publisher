18849387f9a33d92dd9a0077489df4b119fd4db8
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-30 22:42:43 -0400

    Issues now pagination friendly.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -54,8 +54,8 @@ class IssuesController < ApplicationController
     
   def index
     @issues = Issue.paginate(:all, :page => (params[:page] || 1), :per_page =>( params[:per_page] || 15),  :account_id => @account.account_resource_id, :as => @account.access_token)
-    @issue_pagination = { :current_page => @issues.first.current_page, :per_page => @issues.first.per_page, :total_entries => @issues.first.total_entries }
-    @issues = @issues.first.issue.collect do |i|
+    @issue_pagination = { :current_page => @issues.current_page, :per_page => @issues.per_page, :total_entries => @issues.total_entries }
+    @issues = @issues.collect do |i|
       i.account.access_token = @account.access_token
       i
     end
