43573ecfd8deae5625a7c0a7a6dbd350ee19cc37
Christopher Dinn <me@christopher-dinns-macbook-pro.local>
2009-08-30 22:38:34 -0400

    Sometimes section articles come back as an array.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -17,7 +17,11 @@ class SectionsController < ApplicationController
 
     
     @articles = Article.paginate(:all, :page => (params[:page] || 1), :per_page => ( params[:per_page] || 15), :account_id => @account.account_resource_id, :section_id => @section.id, :as => @account.access_token)
-    @article_pagination = { :current_page => @articles.current_page, :per_page => @articles.per_page, :total_entries => @articles.total_entries }
+    if @article.is_a?(WillPaginate::Collection)
+      @article_pagination = { :current_page => @articles.current_page, :per_page => @articles.per_page, :total_entries => @articles.total_entries }
+    else
+      @article_pagination = {}
+    end
     
       # Widget data processing -- start  
       # Build query of only the necessary ids, from the widgets      
