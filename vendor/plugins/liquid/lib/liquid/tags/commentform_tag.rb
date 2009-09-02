c5dae0aeca4ee1120729b150669005834461d421
Andrew Louis <andrew@hyfen.net>
2009-09-02 13:25:28 -0400

    Commented out form for now

--- vendor/plugins/liquid/lib/liquid/tags/commentform_tag.rb
+++ vendor/plugins/liquid/lib/liquid/tags/commentform_tag.rb
@@ -21,25 +21,25 @@ module Liquid
     def render(context)      
 
       
-      s = <<-end
-        <form id="comment-form" method="post" action="/accounts/#{context.registers[:account].id}/articles/#{context.registers[:article].id}/comments/">
-        <input name="authenticity_token" type="hidden" value="#{context.registers[:form_authenticity_token]}" />
-        <input type="hidden" id="comment_content_id" name="comment[content_id]" value="#{context.registers[:article].id" />
-        
-        <label for="comment_body">Comment:</label><br />
-        <textarea rows="10" cols="40"  id="comment_body" name="comment[body]"></textarea><br />
-        
-        <label for="comment_name">Your name:</label><br />
-        <input type="text" id="comment_name" name="comment[name]"/><br />
-        
-        <label for="comment_email">Your email:</label><br />
-        <input type="text" id="comment_email" name="comment[email]"/><br />
-        
-        <label for="comment_url">Your url:</label><br />
-        <input type="text" id="comment_url" name="comment[url]"/><br />
-        
-        <input type="submit" value="Post" />
-      end
+      # s = <<-end
+      #   <form id="comment-form" method="post" action="/accounts/#{context.registers[:account].id}/articles/#{context.registers[:article].id}/comments/">
+      #   <input name="authenticity_token" type="hidden" value="#{context.registers[:form_authenticity_token]}" />
+      #   <input type="hidden" id="comment_content_id" name="comment[content_id]" value="#{context.registers[:article].id" />
+      #   
+      #   <label for="comment_body">Comment:</label><br />
+      #   <textarea rows="10" cols="40"  id="comment_body" name="comment[body]"></textarea><br />
+      #   
+      #   <label for="comment_name">Your name:</label><br />
+      #   <input type="text" id="comment_name" name="comment[name]"/><br />
+      #   
+      #   <label for="comment_email">Your email:</label><br />
+      #   <input type="text" id="comment_email" name="comment[email]"/><br />
+      #   
+      #   <label for="comment_url">Your url:</label><br />
+      #   <input type="text" id="comment_url" name="comment[url]"/><br />
+      #   
+      #   <input type="submit" value="Post" />
+      # end
       
       return s
       
