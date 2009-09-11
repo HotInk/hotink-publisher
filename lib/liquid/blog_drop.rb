504ebf72fb16d230bbe210006771864e663583b1
Chris Dinn <chris@massaudience.ca>
2009-09-11 10:52:19 -0400

    Must remember to initialize drops.

--- lib/liquid/blog_drop.rb
+++ lib/liquid/blog_drop.rb
@@ -1,4 +1,10 @@
 class Liquid::BlogDrop < Liquid::BaseDrop
   liquid_attributes << :title << :description  << :id << :updated_at
   
+  def initialize(source, options = {})
+     super source
+     @account = source.account unless @account
+     @options  = options
+   end
+  
 end
