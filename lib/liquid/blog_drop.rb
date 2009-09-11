f4154bc31f411a2f5c69b7fc7df7fcf56c97a23f
Chris Dinn <chris@massaudience.ca>
2009-09-11 12:50:43 -0400

    update liquid drop for blogs.

--- lib/liquid/blog_drop.rb
+++ lib/liquid/blog_drop.rb
@@ -1,10 +1,14 @@
 class Liquid::BlogDrop < Liquid::BaseDrop
-  liquid_attributes << :title << :description  << :id << :updated_at
+  liquid_attributes << :title << :description << :updated_at
   
   def initialize(source, options = {})
      super source
      @account = source.account unless @account
      @options  = options
+   end
+   
+   def id
+     source.id
    end
   
 end
