5de1eb99b5b3336d17842867dddaf503886e8b15
Chris Dinn <chris@massaudience.ca>
2009-09-14 11:00:13 -0400

    Added blog to article drop.

--- lib/liquid/article_drop.rb
+++ lib/liquid/article_drop.rb
@@ -14,6 +14,11 @@ class Liquid::ArticleDrop < Liquid::BaseDrop
       'bodytext' => @source.bodytext
   end
   
+  # If this article is a blog entry, make the blog available.
+  def blog
+    source.blog
+  end
+  
   def authors_list
     source.authors_list
   end
