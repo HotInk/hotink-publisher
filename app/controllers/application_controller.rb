a7519ebe5f5f5c791a702ce7303626aa12a587d9
Chris Dinn <chris@massaudience.ca>
2009-09-16 15:14:24 -0400

    Move zissou to html file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -170,7 +170,7 @@ class ApplicationController < ActionController::Base
     end
     
     def zissou
-      render :text => "<img src=\"/images/zissou.jpg\" /><h1>Out here we call them 404s, Ned</h1><p>Sorry, this page doesn't exist.</p> ", :status => :not_found
+      render :file => "#{RAILS_ROOT}/public/zissou.html", :status => :not_found
       return
     end
     
