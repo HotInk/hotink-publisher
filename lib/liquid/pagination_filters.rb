aa518f394e9f267b109a8485c5998e20e5c8fe08
Chris Dinn <chris@massaudience.ca>
2009-09-18 12:42:09 -0400

    Added search pagination.

--- lib/liquid/pagination_filters.rb
+++ lib/liquid/pagination_filters.rb
@@ -64,6 +64,27 @@ module Liquid
       pagination_html
     end
     
+    # Search pagination doesn't expect pages to be sorted by date.
+    def search_paginate(pagination_info)
+      pagination_html = ""
+      if pagination_info.is_a?(Hash) && (pagination_info["current_page"] && pagination_info["per_page"] && pagination_info["total_entries"])
+       
+        pagination_html += "<div class=\"pagination\">"
+        
+        # Previous link, if appropriate
+        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" class=\"prev_page\" rel=\"prev\">&laquo; Previous</a>" if pagination_info["current_page"].to_i > 1 
+
+        # Next link, if appropriate
+        if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
+          pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" class=\"next_page\" rel=\"next\">Next &raquo;</a>"
+        end
+        
+        pagination_html += "</div>"
+        
+      end
+      pagination_html
+    end
+    
     
   end
 
