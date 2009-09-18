fdde28ae275fd179df4321c3201b9c203e1dff0a
Chris Dinn <chris@massaudience.ca>
2009-09-18 12:51:19 -0400

    Now pagination variable names even more consistent.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,15 +27,15 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id], :account_id => @account.account_resource_id, :as => @account.access_token)
     @entries = Entry.paginate(:all, :from => "/accounts/#{@account.account_resource_id.to_s}/blogs/#{@blog.id.to_s}/entries.xml", :params => { :page => (params[:page] || 1), :per_page => ( params[:per_page] || 15) }, :as => @account.access_token )
     if @entries.first.respond_to?(:current_page) && @entries.first.respond_to?(:article)
-        @entrie_pagination = { 'current_page' => @entries.first.current_page, 'per_page' => @entries.first.per_page, 'total_entries' => @entries.first.total_entries }
+        @entries_pagination = { 'current_page' => @entries.first.current_page, 'per_page' => @entries.first.per_page, 'total_entries' => @entries.first.total_entries }
         @entries = @entries.first.article
     else
         @entries_pagination = {}
         @entries = []
     end
-    page_html = @current_template.parsed_code.render({'blog' => @blog, 'entries' => @entries, 'newspaper' => @newspaper}, :registers => @registers )
+    page_html = @current_template.parsed_code.render({'blog' => @blog, 'entries' => @entries, 'entries_pagination' => @entries_pagination, 'newspaper' => @newspaper}, :registers => @registers )
     if @current_template.current_layout
-      render :text => @current_template.current_layout.parsed_code.render({'page_content' => page_html, 'newspaper' => @newspaper}, :registers => @registers)
+      render :text => @current_template.current_layout.parsed_code.render({'page_content' => page_html, 'entries_pagination' => @entries_pagination, 'newspaper' => @newspaper}, :registers => @registers)
     else  
       render :text => page_html
     end
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -99,7 +99,7 @@ class IssuesController < ApplicationController
     @registers[:account] = @account
     @registers[:design] = @current_template.design if @current_template.design
 
-    page_html = @current_template.parsed_code.render({'issues' => @issues.to_a, 'issue_pagination' => @issue_pagination,  'newspaper' => @newspaper}, :registers => @registers )
+    page_html = @current_template.parsed_code.render({'issues' => @issues.to_a, 'issues_pagination' => @issues_pagination,  'newspaper' => @newspaper}, :registers => @registers )
      if @current_template.current_layout
        render :text => @current_template.current_layout.parsed_code.render({'page_content' => page_html, 'issues' => @issues.to_a, 'issue_pagination' => @issue_pagination, 'newspaper' => @newspaper}, :registers => @registers)
      else  
