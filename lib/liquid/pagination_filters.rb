2fb9cabface6f712473e8ed4f98a8d149a752aee
Chris Dinn <chris@massaudience.ca>
2009-09-18 11:56:17 -0400

    Add current page and pagination window to pagination filter.

--- lib/liquid/pagination_filters.rb
+++ lib/liquid/pagination_filters.rb
@@ -9,12 +9,18 @@ module Liquid
         pagination_html += "<div class=\"pagination\">"
         
         # Previous link, if appropriate
-        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" class=\"prev_page\" rel=\"prev\">&laquo; Previous</a>" if pagination_info["current_page"].to_i > 1 
+        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" class=\"prev_page\" rel=\"prev\">&laquo; Newer</a>" if pagination_info["current_page"].to_i > 1 
 
-
+        
+        # Current page and pagination window
+        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" rel=\"prev\">#{pagination_info["current_page"].to_i - 1}</a>" if pagination_info["current_page"] > 1
+        pagination_html += "<span class=\"current\">#{pagination_info["current_page"]}</span>"
+        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" rel=\"next\">#{pagination_info["current_page"].to_i + 1}</a>" if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
+        
+        
         # Next link, if appropriate
         if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
-          pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" class=\"next_page\" rel=\"next\">Next &raquo;</a>"
+          pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" class=\"next_page\" rel=\"next\">Older &raquo;</a>"
         end
         
         pagination_html += "</div>"
