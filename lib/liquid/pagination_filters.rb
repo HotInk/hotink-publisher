2e2897144ac4b1381c5a11876b3750efe1431c93
Chris Dinn <chris@massaudience.ca>
2009-09-18 10:48:26 -0400

    Add next link to pagination.

--- lib/liquid/pagination_filters.rb
+++ lib/liquid/pagination_filters.rb
@@ -4,10 +4,17 @@ module Liquid
   
     def paginate(pagination_info)
       pagination_html = ""
-      if pagination_info.is_a? Hash
-        if pagination_info["current_page"] && (pagination_info["current_page"].to_i > 1) 
-          pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" class=\"prev_page\" rel=\"prev\">&laquo; Previous</a> "
-        end  
+      if pagination_info.is_a?(Hash) && (pagination_info["current_page"] && pagination_info["per_page"] && pagination_info["total_entries"])
+
+        # Previous link, if appropriate
+        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" class=\"prev_page\" rel=\"prev\">&laquo; Previous</a>" if pagination_info["current_page"].to_i > 1 
+
+
+        # Next link, if appropriate
+        if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
+          pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" class=\"next_page\" rel=\"next\">Next &raquo;</a>"
+        end
+        
       end
       pagination_html
     end
