04382c77ff3935a5745b3311c90354404f238c77
Chris Dinn <chris@massaudience.ca>
2009-09-18 12:22:24 -0400

    Added short_paginate filter.

--- lib/liquid/pagination_filters.rb
+++ lib/liquid/pagination_filters.rb
@@ -30,7 +30,7 @@ module Liquid
         pagination_html += "<span class=\"current\">#{pagination_info["current_page"]}</span>"
         pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" rel=\"next\">#{pagination_info["current_page"].to_i + 1}</a>" if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
         
-        # Pagination end-window
+        # TODO: Pagination end-window
         
         # Next link, if appropriate
         if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
@@ -42,6 +42,28 @@ module Liquid
       end
       pagination_html
     end
+    
+    
+    def short_paginate(pagination_info)
+      pagination_html = ""
+      if pagination_info.is_a?(Hash) && (pagination_info["current_page"] && pagination_info["per_page"] && pagination_info["total_entries"])
+       
+        pagination_html += "<div class=\"pagination\">"
+        
+        # Previous link, if appropriate
+        pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i - 1}\" class=\"prev_page\" rel=\"prev\">&laquo; Newer</a>" if pagination_info["current_page"].to_i > 1 
+
+        # Next link, if appropriate
+        if (pagination_info["current_page"].to_i * pagination_info["per_page"].to_i) < pagination_info["total_entries"].to_i
+          pagination_html += "<a href=\"?page=#{pagination_info["current_page"].to_i + 1}\" class=\"next_page\" rel=\"next\">Older &raquo;</a>"
+        end
+        
+        pagination_html += "</div>"
+        
+      end
+      pagination_html
+    end
+    
     
   end
 
