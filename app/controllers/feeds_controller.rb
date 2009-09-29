69cc14f770793d313f4b2aa72123dc890a212952
Chris Dinn <chris@massaudience.ca>
2009-09-29 12:28:16 -0400

    De-paginate articles on feeds/show

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,6 +4,14 @@ class FeedsController < ApplicationController
   
   def show
     @articles = Article.find(:all, :account_id => @account.account_resource_id, :per_page => 40, :page=>1, :as => @account.access_token)
+    if @articles.first.respond_to?(:current_page) && @articles.first.respond_to?(:article)
+       @articles_pagination = { 'current_page' => @articles.first.current_page, 'per_page' => @articles.first.per_page, 'total_entries' => @articles.first.total_entries }
+       @articles = @articles.first.article
+    else
+       @articles_pagination = {}
+       @articles = nil
+    end
+    
     @feed_title = (@account.formal_name||@account.name.capitalize) + " news feed"
     @feed_description = "Most recent articles from " + (@account.formal_name||@account.name.capitalize)
     @feed_url = "http://" + request.host_with_port + request.request_uri
