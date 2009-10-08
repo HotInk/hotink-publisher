fc2a4f76949f62d5f6a1a9e75b4b1ac7247315fb
Andrew Louis <andrew@hyfen.net>
2009-10-08 14:49:58 -0400

    Hardcoded redirection URL until we get proper account root urls into the system

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -63,11 +63,7 @@ class CommentsController < ApplicationController
     
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to(account_article_path(@account, @comment.content_id)) }
-        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+        format.html { redirect_to("/articles/"+@comment.content_id) }
       end
     end
   end
