6498ed4d81b36526a49c1a1a97c1973c268a90a3
Chris Dinn <chris@massaudience.ca>
2009-10-09 11:30:43 -0400

    Remove debug string.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -64,7 +64,7 @@ class SectionsController < ApplicationController
     @registers[:account] = @account
     @registers[:design] = @current_template.design if @current_template.design
    
-    page_html = @current_template.parsed_code.render({'current_section' => @section, 'articles' => @articles.to_a, 'articles_pagination' => @articles_pagination, 'newspaper' => @newspaper}, :registers => @registers ) + '<div class="forwarding-servers" style="display:none;">' + request.env.inspect.to_s + '</div>'
+    page_html = @current_template.parsed_code.render({'current_section' => @section, 'articles' => @articles.to_a, 'articles_pagination' => @articles_pagination, 'newspaper' => @newspaper}, :registers => @registers )
      if @current_template.current_layout
        render :text => @current_template.current_layout.parsed_code.render({'page_content' => page_html, 'current_section' => @section, 'articles' => @articles.to_a, 'article_pagination' => @article_pagination, 'newspaper' => @newspaper}, :registers => @registers)
      else  
