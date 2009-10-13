e965f6604919ec00683f01eb2f52648412c6cacf
Andrew Louis <andrew@hyfen.net>
2009-10-13 16:22:39 -0400

    Added captcha answer and question to comment_form.rb {{form.captcha_question}}

--- vendor/plugins/liquid/lib/liquid/tags/comment_form.rb
+++ vendor/plugins/liquid/lib/liquid/tags/comment_form.rb
@@ -25,7 +25,9 @@ module Liquid
              'email'  => %(<input type="text" id="comment_email" name="comment[email]" value="#{submitted['email']}" />),
              'url'    => %(<input type="text" id="comment_url" name="comment[url]" value="#{submitted['url']}" />),
              'captcha_answer'    => %(<input type="text" id="captcha_answer" name="captcha_answer" value="" />),            
+             'captcha_question' => %(#{context.registers[:captcha_question]}),
              'submit' => %(<input type="submit" value="Submit" />)
+
           }
           
           result << %(<form id="comment-form" method="post" action="#{context.registers[:account].url}/articles/#{context['article'].id}/comments"><input name="authenticity_token" type="hidden" value="#{context.registers[:form_authenticity_token]}" /><input name="captcha_id" type="hidden" value="#{context.registers[:captcha_id]}" />#{[errors]+render_all(@nodelist, context)}</form>)
