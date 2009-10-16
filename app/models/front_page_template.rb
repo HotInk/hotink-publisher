60e8f0ad1128889afa1d531a09d7c7d39e1855ef
Chris Dinn <chris@massaudience.ca>
2009-10-16 07:49:20 -0400

    All widgets on front page template now more reliable.

--- app/models/front_page_template.rb
+++ app/models/front_page_template.rb
@@ -9,8 +9,11 @@ class FrontPageTemplate < WidgetTemplate
   # Returns all widgets, not just those belonging to this template
   # but also those belonging to this page's layout
   def all_widgets
-    found_widgets = self.widgets
-    found_widgets += self.current_layout.widgets if self.current_layout
+    if self.current_layout
+      return self.widgets + self.current_layout.widgets
+    else 
+      return self.widgets
+    end
   end
   
 end
