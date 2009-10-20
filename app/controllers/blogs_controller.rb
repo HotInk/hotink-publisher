e84fdb9be176cffd59d5f8a42eb68ee38d4aefcf
Andrew Louis <andrew@hyfen.net>
2009-10-20 14:36:19 -0400

    Bugfix: handle the case where there's only one entry/article returned from the API

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,8 +28,8 @@ class BlogsController < ApplicationController
     @entries = Entry.paginate(:all, :from => "/accounts/#{@account.account_resource_id.to_s}/blogs/#{@blog.id.to_s}/entries.xml", :params => { :page => (params[:page] || 1), :per_page => ( params[:per_page] || 15) }, :as => @account.access_token )
     if @entries.first.respond_to?(:current_page) && @entries.first.respond_to?(:article)
         @entries_pagination = { 'current_page' => @entries.first.current_page, 'per_page' => @entries.first.per_page, 'total_entries' => @entries.first.total_entries }
-        @entries = @entries.first.article
-    else
+        @entries = @entries.first.article.to_a
+      else
         @entries_pagination = {}
         @entries = []
     end
