f2ad42d3c47585c031668b1cb458a5f039c7c82a
Chris Dinn <chris@massaudience.ca>
2009-11-04 09:39:44 -0500

    Only allow Hot Ink account #1 users to do importing, for now.

--- app/controllers/design_imports_controller.rb
+++ app/controllers/design_imports_controller.rb
@@ -5,28 +5,30 @@ class DesignImportsController < ApplicationController
   end
   
   def create
-    @design = Design.new
-    @design_to_import = Design.find_by_id_and_account_id(params[:design_id])
-    @design.attributes = @design_to_import.attributes
-    @design.parent = @design_to_import
-    @design.public = false
-    @design.account = @account
-    @design.save  
+    if current_user.account.id == 1
+      @design = Design.new
+      @design_to_import = Design.find_by_id_and_account_id(params[:design_id])
+      @design.attributes = @design_to_import.attributes
+      @design.parent = @design_to_import
+      @design.public = false
+      @design.account = @account
+      @design.save  
     
-    for template in @design_to_import.templates
-      new_template = template.clone
-      new_template.design = @design
-      new_template.parsed_code = Liquid::Template.parse(template.code)
-      new_template.save
+      for template in @design_to_import.templates
+        new_template = template.clone
+        new_template.design = @design
+        new_template.parsed_code = Liquid::Template.parse(template.code)
+        new_template.save
+      end
+    
+      for template_file in @design_to_import.template_files
+        new_template_file = template_file.clone
+        new_template_file.design = @design
+        new_template_file.file = template_file.file
+        new_template_file.save
+      end
+
     end
-    
-    for template_file in @design_to_import.template_files
-      new_template_file = template_file.clone
-      new_template_file.design = @design
-      new_template_file.file = template_file.file
-      new_template_file.save
-    end
-    
   end
   
   def show
