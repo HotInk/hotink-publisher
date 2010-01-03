d2a754bb5a592b979e985f206074392c198bc5b4
chrisdinn <me@chrisdinn.ca>
2010-01-03 15:21:59 -0500

    Make sure to require logger in SSO.

--- app/metal/sso.rb
+++ app/metal/sso.rb
@@ -1,6 +1,7 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
 require 'sinatra/base'
+require 'logger'
 
 class Sso < Sinatra::Base
   use Keymaster::Client::Middleware do |sso|
