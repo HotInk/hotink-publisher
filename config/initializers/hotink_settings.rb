43fdb2658f22c47ab822cc94d9aaadee16ae1e30
chrisdinn <me@chrisdinn.ca>
2010-01-03 14:43:11 -0500

    Simple change to settings.

--- config/initializers/hotink_settings.rb
+++ config/initializers/hotink_settings.rb
@@ -6,7 +6,7 @@
 # Chris D's Macbook Pro cred
 #OAUTH_CREDENTIALS = { :token => "sh2W3WFO4PWYz8FvBO8g", :secret => "Q2BskVRYyhv5tAEPaevpAav90LRZQ61VpvyId0opI", :site => "http://hotink.theorem.ca" }
 # Chris D's Macbook Pro local Hot Ink instance cred
-OAUTH_CREDENTIALS = { :token => "Ox561thufVrAdMDLOeM2YQ", :secret => "uhXUK2XaCOPEraG3wrvc2JjffyIujr6iQjno6i8Q", :site => "http://0.0.0.0:3000" }
+#OAUTH_CREDENTIALS = { :token => "Ox561thufVrAdMDLOeM2YQ", :secret => "uhXUK2XaCOPEraG3wrvc2JjffyIujr6iQjno6i8Q", :site => "http://0.0.0.0:3000" }
 # Chris D's office iMac dev cred
 #OAUTH_CREDENTIALS = { :token => "HdLS8aGol4BKblPnR8xNA", :secret => "mVMjBHaCZd4bztcN5ANjFdqm10G94Ia529eK3Q2Lc", :site => "http://hotink.theorem.ca"  }
 
