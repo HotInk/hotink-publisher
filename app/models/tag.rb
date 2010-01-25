e20ced6ea0664e59cd87aa2fc863a065491f65f5
Chris Dinn <chrisgdinn@gmail.com>
2010-01-24 23:25:42 -0500

    Add site to tag model.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,8 @@
 class Tag < ActiveResource::Base
-  
+
+  self.site = HOTINK_SETTINGS[:site]
+  self.prefix = "/accounts/:account_id/"
+    
   def to_liquid
     {'name' => self.name, 'id' => self.id }
   end
